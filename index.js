fde13e536308ffad9a5de1019c985530386adb64
MrPinkPanther10 <90013822+MrPinkPanther10@users.noreply.github.com>
2022-01-08 14:08:23 -0500

    added engineer and intern functions

--- index.js
+++ index.js
@@ -88,3 +88,125 @@ function addManager() {
         createTeam()
     });
 }
+
+// Function for Engineer Option
+function addEngineer() {
+    inquirer.prompt([{
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's Name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's employee ID number?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the ID number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email address?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerOfficeNumber",
+            message: "What is the engineer's office number?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the office number!');
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Manager(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerOfficeNumber);
+        teamArray.push(engineer);
+        createTeam()
+    });
+}
+
+// Function for Intern Option
+function addIntern() {
+    inquirer.prompt([{
+            type: "input",
+            name: "internName",
+            message: "What is the intern's Name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the intern's employee ID number?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the ID number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email address?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internOfficeNumber",
+            message: "What is the intern's office number?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the office number!');
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        const intern = new Manager(answers.internName, answers.internId, answers.internEmail, answers.internOfficeNumber);
+        teamArray.push(intern);
+        createTeam()
+    });
+}
